c8b723faebf3367374e3491a5ae7156c61020844
Andrade Sampaio <sampaio.andrade@outlook.com>
2022-06-04 19:22:04 -0300

    Primeiro Projeto em Flutter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(
+          secondary: Colors.blue,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
---
+++ lib/screens/transferencia/lista.dart
@@ -0,0 +1,65 @@
+import 'package:bytebank/models/transferencia.dart';
+import 'package:bytebank/screens/transferencia/formulario.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Transferências';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = [];
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciasState();
+  }
+}
+
+class ListaTransferenciasState extends State<ListaTransferencias> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(_tituloAppBar),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, indice) {
+          final transferencia = widget._transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencia();
+          })).then(
+            (transferenciaRecebida) => _atualiza(transferenciaRecebida),
+          );
+        },
+      ),
+    );
+  }
+
+  void _atualiza(Transferencia transferenciaRecebida) {
+    setState(() {
+      widget._transferencias.add(transferenciaRecebida);
+    });
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: const Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
